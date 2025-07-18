c5c573e21ae66bb55cf6cfe94cf45f19d1ffdb0e
nagarithwik <nagarithwikv@gmail.com>
2025-07-18 10:13:08 +0530

    bmi

---
+++ BMI.py
@@ -0,0 +1,25 @@
+# Username Generator
+real_name=input("Enter your  name: ")
+nick_name=input("Enter your  nick name: ")
+sentence= f'your username is {real_name}_{nick_name}'
+print(sentence)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
