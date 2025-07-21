f3ee6f0b21fd7e9888db47569f919dc687500403
rit894 <NAGARITHWIKV@GMAIL.COM>
2025-07-21 09:06:45 +0530

    Update 1 project .py

--- 1 project .py
+++ 1 project .py
@@ -1,72 +1,72 @@
 # Initial input
-#x = input("Please enter what you want to do today: ")
-#y = [x]
-#print(y)
+x = input("Please enter what you want to do today: ").strip()
+y = [x]
+print("Your initial list:", y)
+
 # Counters and limits
-#add_count = 0
-#remove_count = 0
-#limit = 5
+add_count = 1
+remove_count = 0
+limit = 5
 
 # Adding items
-#while True:
-   # if add_count >= limit:
-       # extend_add = input("You've reached the limit of 5 additions. Do you want to extend the limit? (yes/no): ").strip().lower()
-      #  if extend_add == "yes":
-     #       extra = int(input("How many more items would you like to add? "))
-    #        limit += extra
-   #     else:
-   #         print("Okay, no more additions allowed.")
-  #          break
-##
-   # add = input("Do you want to add anything else? (yes/no): ").strip().lower()
+while True:
+    if add_count >= limit:
+        extend_add = input("You've reached the limit of additions. Extend it? (yes/no): ").strip().lower()
+        if extend_add == "yes":
+            extra = int(input("How many more items would you like to add? "))
+            limit += extra
+        else:
+            print("No more additions allowed.")
+            break
 
-    #if add == "yes":
-     #   print("CAUTION: PLEASE ENTER ONE THING AT A TIME")
-    #    addi = input("What would you like to add to the list? ")
-       # y.append(addi)
-      #  add_count += 1
-     #   print("Updated list:", y)
-    #elif add == "no":
-    #    quit_add = input("Do you want to quit adding? (yes/no): ").strip().lower()
-    #    if quit_add == "yes":
-   #         break
-  #  else:
- #       print("Please answer with 'yes' or 'no'.")
+    add = input("Do you want to add anything else? (yes/no): ").strip().lower()
+    if add == "yes":
+        print("CAUTION: PLEASE ENTER ONE THING AT A TIME")
+        addi = input("What would you like to add to the list? ").strip()
+        y.append(addi)
+        add_count += 1
+        print("Updated list:", y)
+    elif add == "no":
+        quit_add = input("Do you want to quit adding? (yes/no): ").strip().lower()
+        if quit_add == "yes":
+            break
+    else:
+        print("Please answer with 'yes' or 'no'.")
 
 # Removing items
-#while True:
-    #if remove_count >= limit:
-    #    extend_remove = input("You've reached the limit of 5 removals. Do you want to extend the limit? (yes/no): ").strip().lower()
-    #    if extend_remove == "yes":
-    #        extra = int(input("How many more items would you like to remove? "))
-    #        limit += extra
-   #     else:
-  #          print("Okay, no more removals allowed.")
- #           break
+while True:
+    if remove_count >= limit:
+        extend_remove = input("You've reached the limit of removals. Extend it? (yes/no): ").strip().lower()
+        if extend_remove == "yes":
+            extra = int(input("How many more items would you like to remove? "))
+            limit += extra
+        else:
+            print("No more removals allowed.")
+            break
 
-#    remove = input("Do you want to remove anything? (yes/no): ").strip().lower()
-
-    #if remove == "yes":
-    #    remove_item = input("What would you like to remove from the list? ")
-    #    if remove_item in y:
-    #        y.remove(remove_item)
-    #        remove_count += 1
-    #        print("Updated list:", y)
-    #    else:
-    #        print(f"'{remove_item}' is not in the list.")
-    #elif remove == "no":
-    #   quit_remove = input("Do you want to quit removing? (yes/no): ").strip().lower()
-    #    if quit_remove == "yes":
-   #         break
-  #  else:
- #       print("Please answer with 'yes' or 'no'.")
+    remove = input("Do you want to remove anything? (yes/no): ").strip().lower()
+    if remove == "yes":
+        remove_item = input("What would you like to remove from the list? ").strip()
+        if remove_item in y:
+            y.remove(remove_item)
+            remove_count += 1
+            print("Updated list:", y)
+        else:
+            print(f"'{remove_item}' is not in the list.")
+    elif remove == "no":
+        quit_remove = input("Do you want to quit removing? (yes/no): ").strip().lower()
+        if quit_remove == "yes":
+            break
+    else:
+        print("Please answer with 'yes' or 'no'.")
 
 # Final summary
-#print("\n✅ Final Summary:")
-#print("Total number of items added:", add_count)
-#print("Total number of items removed:", remove_count)
-#print("Here is your final to-do list:", y)
-
+print("\nFinal Summary:")
+print("Total number of items added:", add_count)
+print("Total number of items removed:", remove_count)
+print("Here is your final to-do list:", y)
+  
+  
 
 
 
